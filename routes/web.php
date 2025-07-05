7e6a5cc6ed2cc8e8f419a2f216379b7a767f13ef
iamjnayem <iamj.nayem@gmail.com>
2025-07-05 16:29:39 +0600

    update app service provider & web route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
 	return view('react');
 });
 
+Route::get('/test-env', function () {
+    dd(env('APP_URL'), config('app.url'));
+});
+
 Route::fallback(function () {
     return view('react'); // Ensure this points to your React app's entry point
 });
