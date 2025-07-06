a6e0eca4e697735be3ca808bc4c19a9969384e2d
jannatulnayem <jannatulnayem@sslwireless.com>
2025-07-06 14:06:27 +0600

    update trusted config

--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -2,5 +2,5 @@
 
 return [
     'proxies' => '*', // Trust all proxies (or use array of IPs)
-    'headers' => \Illuminate\Http\Middleware\TrustProxies::HEADER_X_FORWARDED_ALL,
+    'headers' => 'HEADER_X_FORWARDED_ALL',
 ];
